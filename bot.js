cb7d8afcbff2b47312d0527fc0e9f078427cbd16
Nahida4479 <szymon.cyb13@gmail.com>
2026-08-03 00:58:41 +0200

    Create clead file system and auto create downloads file

--- bot.js
+++ bot.js
@@ -1,5 +1,26 @@
 import 'dotenv/config';
 import { Client, GatewayIntentBits, SlashCommandBuilder } from 'discord.js';
+import fs from 'fs';
+import { error } from 'console';
+
+const downloadsFolder = 'downloads/';
+
+if (!fs.existsSync(downloadsFolder)) {
+    try {
+        fs.mkdirSync(downloadsFolder);
+        console.log('Created folder:', downloadsFolder);
+    } catch (error) {
+        console.log('Could not create downloads folder. Check permissions.', error);
+    }
+}
+
+const existingFiles = fs.readdirSync(downloadsFolder);
+existingFiles.forEach(function(filename) {
+    fs.unlink(downloadsFolder);
+});
+console.log('Cleaned up', existingFiles.length, 'old file from', downloadsFolder);
+
+
 
 process.on('unhandledRejection', function(error){
     console.error("Unhandled promise rejection:", error)
@@ -8,6 +29,8 @@ process.on('unhandledRejection', function(error){
 process.on('uncaughtException', function(error) {
     console.error("Uncaught exception:", error);
 });
+
+
 
 
 const client = new Client({
