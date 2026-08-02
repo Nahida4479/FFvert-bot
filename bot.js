1d326488b66761e404a943d57fea7977293e3b6e
Nahida4479 <szymon.cyb13@gmail.com>
2026-08-03 00:27:21 +0200

    Create /convert command

--- bot.js
+++ bot.js
@@ -1,14 +1,26 @@
-require('dotenv').config;
-const { Client, GatewayIntentBits } = require('discord.js');
-
+import 'dotenv/config';
+import { Client, GatewayIntentBits, SlashCommandBuilder } from 'discord.js';
 const client = new Client({
     intents: [GatewayIntentBits.Guilds]
 });
 
-client.once('ready', function() {
+client.once('clientReady', async function() {
     console.log(`Logged in as ${client.user.tag}`);
+    await client.application.commands.set([ConvertCommand]);
+    console.log('Slash commands registered successfully');
 });
 
 
 
-client.login(process.env.BOT);
+// /convert
+const ConvertCommand = new SlashCommandBuilder()
+    .setName('convert')
+    .setDescription('Convert an image or video to a different format')
+    .addAttachmentOption(option =>
+        option.setName('file')
+            .setDescription("The image or video file to convert")
+            .setRequired(true)  
+    );
+
+
+client.login(process.env.BOT_TOKEN);    
